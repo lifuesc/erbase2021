55d1ed00a4d778b94abb35d8846ff57b264ad3cb
lifuesc <75270382+lifuesc@users.noreply.github.com>
2021-10-25 21:44:49 -0300

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -14,30 +14,15 @@ app.use(express.json());
 
 // Váriavel que contem as leitura
 const readings = [];
-// Hash de validação
-const hashSensor = "d5b863c277fb1c6475eebef50278ff59";
+
 // Rota de inserção de dados
 app.post("/insert-data", function (req, res) {
   // Pega as variáveis no corpo da requisição
-  var { hash, temperature, humidity } = req.body;
-  // Recebe o timestamp (data e horário) da requisição
-  const timestamp = new Date().getTime();
-
+  const { value, local } = req.body;
   try {
-    // Se o hash não for igual ao do sensor
-    if (hashSensor !== hash) throw { message: "Inválid Hash" };
-    // Verifica se os dados foram enviado
-    // Se vazio retorna erro
-    if (!temperature && !humidity)
-      throw { message: "Empty temperature or humidity" };
+    const time = new Date().getTime();
+    readings.push({ value, time, local });
 
-    // Remove o ponto flutuante multiplicando por 100.
-    // Os dois ultimos digitos são as casas decimais
-    // ! Solidity não aceita float
-    temperature = temperature.toFixed(2) * 100;
-    humidity = humidity.toFixed(2) * 100;
-    // Coloca no final do array os dados
-    readings.push({ temperature, humidity, timestamp });
     // Retorna status de sucesso
     res.status(201).json({ message: "Success!" });
   } catch (error) {
