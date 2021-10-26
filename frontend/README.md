ee3a4a68d3dbc8d2986f31e8a35358c54c4a7668
lifuesc <75270382+lifuesc@users.noreply.github.com>
2021-10-25 21:48:33 -0300

    Add files via upload

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# Interface contrato sensor
+# DApp contrato vacina
 
 ## Instalação
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
-// Configuração para requisições na rede
-import web3 from "./web3";
+
 // Informação do contrato
-import sensor from "./sensor";
-// Módulo para fazer requisições para API
+import vacina from "./contracts/vacina.contract";
+// Configuração para requisições na rede
+import web3 from "./contracts/web3";
+// Estilização
+import "./style.css";
 import axios from "axios";
 
 // Verifica se o numero é menor que 10 e poe um 0 na frente
 const checkZero = (val) => {
   return val < 10 ? "0" + val : val;
 };
-// Função que transforma timestamp em dd/mm/yyyy hh:mm:ss
-const converteHorario = (timestamp) => {
-  var date = new Date(timestamp);
+// Função que transforma time em dd/mm/yyyy hh:mm:ss
+const converteHorario = (time) => {
+  var date = new Date(time);
   var hours = date.getHours();
   var minutes = "0" + date.getMinutes();
   var seconds = "0" + date.getSeconds();
@@ -33,115 +35,251 @@ const converteHorario = (timestamp) => {
 
   return formattedTime;
 };
-// Deixa o decimanl com apenas duas casas
-const convertDecimal = (val) => (val / 100).toFixed(2);
-
-const App = () => {
-  const [readings, setReadings] = useState([]);
-  const [contractReadings, setContractReadings] = useState([]);
-  const [transaction, setTransaction] = useState();
+function Home() {
+  // Origem da vacina
+  const [origem, setOrigem] = useState("");
+  // Nome/tipo da vacina
+  const [nome, setNome] = useState("");
+  // Destino da vacina
+  const [destino, setDestino] = useState("");
+  // Duração do transporte
+  const [duracao, setDuracao] = useState("");
+  // Temperatura máxima da vacina
+  const [tempMax, setTempMax] = useState("");
+  // Temperatura mínima da vacina
+  const [tempMin, setTempMin] = useState("");
+  // Condição vacina
+  const [condicao, setCondicao] = useState(true);
+  // Histórico de registro
+  const [register, setRegister] = useState([]);
+  // Registro de perda
+  const [lostRegister, setLostRegister] = useState([]);
+  // Mensagem de carregando
   const [loading, setLoading] = useState(false);
+  // Informação da transação
+  const [transaction, setTransaction] = useState();
+  // Dados da API
+  const [apiData, setApiData] = useState([]);
+  // Dados da API selecionado
+  const [selectedApiData, setSelectedApiData] = useState("");
+  // Pega dados da API
+  const getDataAPI = async () => {
+    try {
+      // Faz requisição GET para rota da API
+      const response = await axios.get("http://localhost:3333/get-data");
+      // Insere dados retornados no apiData
+      setApiData(response.data.data);
+    } catch (error) {
+      // Mostra erro
+      console.log(error);
+    }
+  };
 
-  // Pega dados do middleware e atribui a Readings
-  const pegarDadosMiddleware = async () => {
+  const pegaRegistroPerda = async () => {
     try {
-      let response = await axios.get("http://localhost:3333/get-data");
-      setReadings(response.data.data);
+      // Pega informações do contrato
+      const _perdaVacina = await vacina.methods.perdaVacina().call();
+
+      // Altera o valor da Condição vacina
+      setLostRegister(_perdaVacina);
     } catch (error) {
       console.log(error);
     }
   };
-  const enviarLeituras = async () => {
+  // Pega informações do contrato
+  const pegaInfoContrato = async () => {
     try {
-      // Altera o estado do loading para true
+      // Pega informações do contrato
+      const _nome = await vacina.methods.nome().call();
+      const _origem = await vacina.methods.origem().call();
+      const _destino = await vacina.methods.destino().call();
+      const _duracao = await vacina.methods.duracao().call();
+      const _tempMax = await vacina.methods.tempMax().call();
+      const _tempMin = await vacina.methods.tempMin().call();
+      const _condicao = await vacina.methods.condicao().call();
+      // Altera o valor do Nome/tipo da vacina
+      setNome(_nome);
+      // Altera o valor da Origem da vacina
+      setOrigem(_origem);
+      // Altera o valor da Destino da vacina
+      setDestino(_destino);
+      // Altera o valor da Duração do transporte
+      setDuracao(_duracao);
+      // Altera o valor da Temperatura máxima da vacina
+      setTempMax(_tempMax);
+      // Altera o valor da Temperatura mínima da vacina
+      setTempMin(_tempMin);
+      // Altera o valor da Condição vacina
+      setCondicao(_condicao);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Lista registro do contrato
+  const listRegister = async () => {
+    try {
+      // Chama o método do contrato que retorna registros
+      const registro = await vacina.methods.getRegistro().call();
+
+      // Insere na variável que é renderizado no gráfico
+      setRegister(registro);
+    } catch (error) {
+      // Mostra erro
+      console.log(error);
+    }
+  };
+  // Insere registro que está cadastrado no servidor
+  const insertRegister = async (e) => {
+    e.preventDefault();
+    try {
+      // Ativa mensagem de carregamento
       setLoading(true);
-      // Pega a última leitura
-      const data = readings[readings.length - 1];
-      // Pega contas do metamask
+      // Pega as contas do metamask
       const contas = await web3.eth.getAccounts();
-      // Insere medidas no contrato passando a temperatura, humidade e o timestamp
-      const leitura = await sensor.methods
-        .insertMeasure([
-          [
-            [data.temperature.toFixed(0), data.humidity.toFixed(0)],
-            data.timestamp,
-          ],
-        ])
-        .send({ from: contas[0] });
-      // Altera o estado da transaction com o valor da leitura
-      setTransaction(leitura);
-      // Volta estado de loading para false
+      // Não existir valor no apiData não será possivel rodar essa função
+      if (apiData.length === 0) throw Error("Dados do servidor não encontrado");
+
+      const idxSelected = parseInt(selectedApiData) - 1;
+      // Pega value, time e local do array apiData
+      const _value = apiData[idxSelected].value;
+      const _time = apiData[idxSelected].time;
+      const _local = apiData[idxSelected].local;
+      // Insere dados de registro
+      const responseTrx = await vacina.methods
+        .insertRegistro(_value, _time, _local)
+        .send({
+          // Diz a carteira que está enviando os dados
+          from: contas[0],
+        });
+      // Mostra os dados da transação
+      setTransaction(responseTrx);
+      // Recarrega dados
+      listRegister();
+      pegaInfoContrato();
+      pegaRegistroPerda();
+      // Retira mensagem de carregando
       setLoading(false);
     } catch (error) {
-      // Volta estado de loading para false
+      // Retira mensagem de carregando
       setLoading(false);
+      // Mostra erro
       console.log(error);
     }
   };
-  const pegarLeituras = async () => {
-    try {
-      // Altera o estado do loading para true
-      setLoading(true);
-      // Pega medidas
-      const leitura = await sensor.methods.getAllMeasure().call();
-      // Altera o estado do contract readings com o valor da leitura
-      setContractReadings(leitura);
-      console.log(leitura);
-      // Volta estado de loading para false
-      setLoading(false);
-    } catch (error) {
-      // Volta estado de loading para false
-      setLoading(false);
-      console.log(error);
-    }
-  };
-  // Chama seu conteúdo antes do código ser renderizado
+
+  // Antes da página ser carregada chama as funções dentro do useEffect
   useEffect(() => {
-    // Quando a pagina inicial ele chama essa função
-    pegarDadosMiddleware();
+    getDataAPI();
+    pegaInfoContrato();
+    listRegister();
+    pegaRegistroPerda();
   }, []);
+
   return (
     <div>
-      <h1>Contrato de sensor</h1>
+      <h1>Sistema de Rastreamento de Vacinas</h1>
+      <div className="divider" />
+      <h2>{nome}</h2>
+      <h2>
+        Contrato:{" "}
+        <a
+          href={
+            "https://rinkeby.etherscan.io/address/" + vacina.options.address
+          }
+          target="_blank"
+          rel="noreferrer"
+        >
+          {vacina.options.address}
+        </a>
+      </h2>
+      <div id="form">
+        <div className="form-div">
+          <label>Origem</label>
+          <input type="text" disabled value={origem} />
+
+          <label>Estimativa de tempo</label>
+          <input type="text" disabled value={duracao} />
+        </div>
+        <div className="form-div">
+          <label>Destino</label>
+          <input type="text" disabled value={destino} />
+
+          <label>Condição da vacina</label>
+          <input
+            type="text"
+            disabled
+            className={condicao ? "success" : "error"}
+            value={condicao ? "Adequada" : "Imprópria"}
+          />
+        </div>
+        <div className="form-div">
+          <label>Temperatura máxima</label>
+          <input type="text" disabled value={tempMax} />
+        </div>
+        <div className="form-div">
+          <label>Temperatura mínima</label>
+          <input type="text" disabled value={tempMin} />
+        </div>
+      </div>
+      <br />
+      <br />
+      <h2>Violação de Temperatura</h2>
+      {lostRegister && lostRegister.local ? (
+        <div className="center">
+          <div className="form-div3">
+            <label>Temperatura</label>
+            <input type="text" disabled value={lostRegister.value} />
+            <label>Local</label>
+            <input type="text" disabled value={lostRegister.local} />
+            <label>Data</label>
+            <input
+              type="text"
+              disabled
+              value={converteHorario(parseInt(lostRegister.time))}
+            />
+          </div>
+        </div>
+      ) : (
+        <h3>Nenhuma</h3>
+      )}
+      <div className="divider" />
+
+      <br />
+      <br />
+      <h2>Registros no contrato</h2>
       <table>
         <thead>
           <tr>
             <th>id</th>
-            <th>Temperatura</th>
-            <th>Umidade</th>
-            <th>Data</th>
+            <th>Valor</th>
+            <th>Time</th>
+            <th>Local</th>
           </tr>
         </thead>
         <tbody>
           {/* Mostra informação das leituras */}
-          {readings.map((reading, idx) => (
+          {register.map((regis, idx) => (
             <tr key={idx}>
               <td>{idx + 1}</td>
-              <td>{convertDecimal(reading.temperature)}</td>
-              <td>{convertDecimal(reading.humidity)}</td>
-              <td>{converteHorario(reading.timestamp)}</td>
+              <td>{regis.value}</td>
+              <td>{converteHorario(parseInt(regis.time))}</td>
+              <td>{regis.local}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      {/* Se leituras maior que zero */}
-      {readings.length > 0 ? (
-        <>
-          <h4>
-            Deseja enviar para o contrato a última visita de id:{" "}
-            {readings.length}?
-          </h4>
-          <button onClick={() => enviarLeituras()}>Sim!</button>
-        </>
-      ) : (
-        <h4>Nenhuma leitura cadastrada no servidor</h4>
-      )}
-      {/* Mostra carregando  */}
+      {!(register.length > 0) ? <h4>Nenhum registro no contrato</h4> : null}
+
+      <br />
+      <br />
+
       {loading ? (
-        <h1>Carregando...</h1>
+        <h1>Cadastrando registro...</h1>
       ) : transaction ? (
         <>
           {/* Mostra informação da transação  */}
+          <h2>Informação da última transação</h2>
           <h3>Endereço de envio: {transaction.from}</h3>
           <h3>Endereço do Contrato: {transaction.to}</h3>
           <h3>
@@ -155,37 +293,46 @@ const App = () => {
           </h3>
         </>
       ) : null}
+      <br />
+      <br />
+      <div className="divider" />
 
-      <br />
-      <h2>Medições do contrato</h2>
-
-      <button onClick={() => pegarLeituras()}>
-        Carregar leituras do contrato
-      </button>
-      <br />
-      <br />
+      <h2>Dados cadastrados no servidor</h2>
       <table>
         <thead>
           <tr>
             <th>id</th>
-            <th>Temperatura</th>
-            <th>Umidade</th>
-            <th>Data</th>
+            <th>Valor</th>
+            <th>Time</th>
+            <th>Local</th>
           </tr>
         </thead>
         <tbody>
-          {contractReadings.map((contractReading, idx) => (
+          {/* Mostra informação das leituras */}
+          {apiData.map((data, idx) => (
             <tr key={idx}>
               <td>{idx + 1}</td>
-              <td>{convertDecimal(contractReading.value[0])}</td>
-              <td>{convertDecimal(contractReading.value[1])}</td>
-              <td>{converteHorario(parseInt(contractReading.timestamp))}</td>
+              <td>{data.value}</td>
+              <td>{converteHorario(parseInt(data.time))}</td>
+              <td>{data.local}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      {!(apiData.length > 0) ? <h4>Nenhum registro no contrato</h4> : null}
+      <div className="form-div2">
+        <label>Selecione o id que deseja enviar para o contrato</label>
+        <input
+          type="text"
+          value={selectedApiData}
+          onChange={(e) => setSelectedApiData(e.target.value)}
+        />
+        <button onClick={insertRegister}>Enviar</button>
+      </div>
+      <br />
+      <br />
     </div>
   );
-};
+}
 
-export default App;
+export default Home;
